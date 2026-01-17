787df66d94bf25cc8d4e032eb5f48672e4aa5a83
anurag <anagkirti@gmail.com>
2026-01-17 10:40:46 +0530

    First commit to add module directory on github with vpc module

---
+++ modules/vpc/main.tf
@@ -0,0 +1,9 @@
+##VPC
+resource "aws_vpc" "my_master_vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    tags = {
+      Name = "my_master_vpc"
+    }
+}
